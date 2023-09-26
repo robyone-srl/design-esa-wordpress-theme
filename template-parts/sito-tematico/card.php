3d38750b9da271a811ce021e94af1a51ef092ba1
Angelo Bottazzo <angelo.bottazzo@robyone.net>
2023-09-26 13:38:15 +0200

    colore personalizzato sito tematico

--- template-parts/sito-tematico/card.php
+++ template-parts/sito-tematico/card.php
@@ -1,5 +1,5 @@
 <?php
-global $sito_tematico_id, $count; 
+global $sito_tematico_id;
 
 $sito_tematico = get_post($sito_tematico_id);
 $prefix = '_dci_sito_tematico_';
@@ -8,14 +8,11 @@ $st_link = dci_get_meta('link',$prefix, $sito_tematico->ID);
 $st_colore = dci_get_meta('colore',$prefix, $sito_tematico->ID);
 $st_img = dci_get_meta('immagine',$prefix, $sito_tematico->ID);
 
-if ($count % 3 == 0) $bg_color = 'blue';
-if ($count % 3 == 1) $bg_color = 'warning';
-if ($count % 3 == 2) $bg_color = 'dark';
-
 $mostra_sfondo = dci_get_option('siti_tematici_sfondi', 'homepage') ?? false;
+$colore_sfondo = dci_get_meta('colore',$prefix, $sito_tematico->ID) ?? false;
 ?>
 
-<a href="<?php echo $st_link ?>" class="card card-teaser card-bg-<?= $mostra_sfondo ? $bg_color: '' ?> rounded mt-0 p-3 shadow-sm border border-light" target="_blank">
+<a href="<?php echo $st_link ?>" style="<?= ($mostra_sfondo && $colore_sfondo) ? 'background-color:'.$colore_sfondo : '' ?>" class="card card-teaser <?= $mostra_sfondo && !$colore_sfondo ? 'bg-primary' : '' ?> rounded mt-0 p-3 shadow-sm border border-light" target="_blank">
     <?php if($st_img) { ?>
         <div class="avatar size-lg me-3">
             <?php dci_get_img($st_img); ?>
