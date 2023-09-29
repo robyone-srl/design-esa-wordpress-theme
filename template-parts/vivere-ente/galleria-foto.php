24f6128cb0cc4188fb2595fe9b4166e92520ca47
Angelo Bottazzo <angelo.bottazzo@robyone.net>
2023-09-29 16:52:35 +0200

    titolo personalizabile

--- template-parts/vivere-ente/galleria-foto.php
+++ template-parts/vivere-ente/galleria-foto.php
@@ -4,19 +4,22 @@ global $sfondo_grigio;
 $sfondo_grigio = $sfondo_grigio ?? true;
 
 $photos = dci_get_option('gallery_items', 'vivi') ?: [];
+$nome_sezione = dci_get_option('gallery_title', 'vivi') ?: [];
 ?>
 
 <?php if (count($photos) > 0) { ?>
-	<section id="galleria">
-		<div class="section <?= $sfondo_grigio ? 'section-muted' : '' ?> px-lg-5 pt-0 pb-0">
+	<section id="galleria" class="pt-5">
+		<?php if ($nome_sezione) { ?>
+		<div class="section <?= $sfondo_grigio ? 'section-muted' : '' ?> px-lg-5 pt-0 py-0">
 			<div class="container">
 				<div class="row row-title pt-5 pt-lg-60 pb-3">
 					<div class="col-12 d-lg-flex justify-content-between">
-						<h2 class="mb-lg-0">Le nostre foto</h2>
+						<h2 class="mb-lg-0"><?= $nome_sezione ?></h2>
 					</div>
 				</div>
 			</div>
 		</div>
+		<?php } ?>
 		<div class="section <?= $sfondo_grigio ? 'section-muted' : '' ?> px-0 pt-0 it-carousel-wrapper it-carousel-landscape-abstract-three-cols it-full-carousel it-standard-image splide" data-bs-carousel-splide>
 			<div class="splide__track">
 				<ul class="splide__list">
