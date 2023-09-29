73df6b11957bc00eba63510b16b8600c24b87b1a
Angelo Bottazzo <angelo.bottazzo@robyone.net>
2023-09-29 17:02:13 +0200

    didascalia

--- template-parts/vivere-ente/galleria-foto.php
+++ template-parts/vivere-ente/galleria-foto.php
@@ -10,15 +10,15 @@ $nome_sezione = dci_get_option('gallery_title', 'vivi') ?: [];
 <?php if (count($photos) > 0) { ?>
 	<section id="galleria" class="pt-5">
 		<?php if ($nome_sezione) { ?>
-		<div class="section <?= $sfondo_grigio ? 'section-muted' : '' ?> px-lg-5 pt-0 py-0">
-			<div class="container">
-				<div class="row row-title pt-5 pt-lg-60 pb-3">
-					<div class="col-12 d-lg-flex justify-content-between">
-						<h2 class="mb-lg-0"><?= $nome_sezione ?></h2>
+			<div class="section <?= $sfondo_grigio ? 'section-muted' : '' ?> px-lg-5 pt-0 py-0">
+				<div class="container">
+					<div class="row row-title pt-5 pt-lg-60 pb-3">
+						<div class="col-12 d-lg-flex justify-content-between">
+							<h2 class="mb-lg-0"><?= $nome_sezione ?></h2>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
 		<?php } ?>
 		<div class="section <?= $sfondo_grigio ? 'section-muted' : '' ?> px-0 pt-0 it-carousel-wrapper it-carousel-landscape-abstract-three-cols it-full-carousel it-standard-image splide" data-bs-carousel-splide>
 			<div class="splide__track">
@@ -26,14 +26,20 @@ $nome_sezione = dci_get_option('gallery_title', 'vivi') ?: [];
 					<?php foreach ($photos as $photo) { ?>
 						<li class="splide__slide">
 							<div class="it-single-slide-wrapper">
-								<div class="card-wrapper">
-									<div class="card card-img no-after">
-										<a class="img-responsive-wrapper" target="_blank" href="<?= $photo ?>">
+								<div class="it-grid-item-wrapper">
+									<a href="<?= $photo ?>" target="_blank">
+										<div class="img-responsive-wrapper">
 											<div class="img-responsive">
 												<?php dci_get_img($photo, 'img-wrapper object-fit-cover'); ?>
 											</div>
-										</a>
-									</div>
+										</div>
+										<span class="it-griditem-text-wrapper">
+											<span class="it-griditem-text"><?= get_the_title(get_post(attachment_url_to_postid($photo))->ID) ?></span>
+											<svg class="icon">
+												<use href="/bootstrap-italia/dist/svg/sprites.svg#it-code-circle"></use>
+											</svg>
+										</span>
+									</a>
 								</div>
 							</div>
 						</li>
