4b7767092db4e26727a6948a4bae7fcc7942106c
Marco Rubin <marco.rubin@robyone.net>
2023-11-30 17:45:33 +0100

    Altre correzioni accessibilità

--- template-parts/common/skiplink.php
+++ template-parts/common/skiplink.php
@@ -1,5 +1,4 @@
-<div class="skiplink">
-    <a class="visually-hidden-focusable" aria-label="vai ai contenuti presenti in pagina" href="#main-container">Vai ai contenuti</a>
-    <a class="visually-hidden-focusable" aria-label="vai al footer" href="#footer">Vai al
-    footer</a>
-</div><!-- /skiplink -->
+<nav class="skiplink" aria-label="Link per saltare ai contenuti">
+    <a class="visually-hidden-focusable" aria-label="Vai ai contenuti presenti in pagina" href="#main-container">Vai ai contenuti</a>
+    <a class="visually-hidden-focusable" aria-label="Vai al footer" href="#footer">Vai al footer</a>
+</nav><!-- /skiplink -->
