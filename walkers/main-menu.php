f9b26ef94321e0c3768ead11eed62fcd9f6fd5e1
Angelo Bottazzo <angelo.bottazzo@robyone.net>
2023-12-07 14:37:49 +0100

    logo mostrato nel menu

--- walkers/main-menu.php
+++ walkers/main-menu.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Nav Menu API: Main_Menu_Walker class
  *
@@ -16,8 +17,12 @@
  */
 
 
-class Main_Menu_Walker extends Walker_Nav_Menu {
-	function start_el(&$output, $item, $depth=0, $args=[], $id=0) {
+class Main_Menu_Walker extends Walker_Nav_Menu
+{
+	function start_el(&$output, $item, $depth = 0, $args = [], $id = 0)
+	{
+		$img_id = get_post_meta($item->ID, 'menu_item_logo', true);
+
 		$output .= "<li class='nav-item'>";
 		// set active tab
 		$group = $args->current_group == 'documenti-e-dati' ? 'amministrazione' : $args->current_group;
@@ -29,19 +34,24 @@ class Main_Menu_Walker extends Walker_Nav_Menu {
 
 		// set data-element for crawler
 		$data_element = '';
-		if ($item->title == 'Amministrazione') $data_element .= 'management'; 
-		if ($item->title == 'Novità') $data_element .= 'news'; 
-		if ($item->title == 'Servizi') $data_element .= 'all-services'; 
-		if ($item->title == 'Vivere il Comune') $data_element .= 'live'; 
- 
+		if ($item->title == 'Amministrazione') $data_element .= 'management';
+		if ($item->title == 'Novità') $data_element .= 'news';
+		if ($item->title == 'Servizi') $data_element .= 'all-services';
+		if ($item->title == 'Vivere il Comune') $data_element .= 'live';
+
 		if ($item->url && $item->url != '#') {
-			$output .= '<a class="nav-link '.$active_class.'" href="' . $item->url . '" data-element="'.$data_element.'">';
+			$output .= '<a class="nav-link ' . $active_class . '" href="' . $item->url . '" data-element="' . $data_element . '">';
 		} else {
 			$output .= '<span>';
 		}
- 
-		$output .= $item->title;
- 
+
+		if ($img_id ?? false)
+			$output .= '<svg width="28" height="28" aria-hidden="true" class="me-2 mb-1">       
+			<image xlink:href="' . wp_get_attachment_image_url($img_id, 'small') . '" width="28" height="28"/>    
+			</svg>';
+
+		$output .= '<span>'.$item->title.'</span>';
+
 		if ($item->url && $item->url != '#') {
 			$output .= '</a>';
 		} else {
