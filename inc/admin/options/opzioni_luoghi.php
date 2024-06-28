ab0038ad42bb6954db93c03fd52b6a7b79da6162
Angelo Bottazzo <angelo.bottazzo@robyone.net>
2024-06-28 12:21:59 +0200

    rimozione opzioni inutilizzate da opzioni luoghi

--- inc/admin/options/opzioni_luoghi.php
+++ inc/admin/options/opzioni_luoghi.php
@@ -20,30 +20,4 @@ function dci_register_luoghi_options(){
     if ( version_compare( CMB2_VERSION, '2.4.0' ) ) {
         $args['display_cb'] = 'dci_options_display_with_tabs';
     }
-
-    $luoghi_options = new_cmb2_box( $args );
-
-    $luoghi_options->add_field( array(
-        'id' => $prefix . 'strutture_luoghi',
-        'name'        => __( 'Seleziona le tipologie di luoghi  da mostrare', 'design_comuni_italia' ),
-        'desc' => __( 'Seleziona le tipologie di luoghi che vuoi mostrare. ' , 'design_comuni_italia' ),
-        'type'           => 'taxonomy_multicheck_hierarchical',
-        'taxonomy'       => 'tipi_luogo',
-        'attributes' => array(
-            'placeholder' =>  __( ' Seleziona e ordina le tipologie di luoghi da mostrare nella pagina Luoghi', 'design_comuni_italia' ),
-            'required'    => 'required'
-        ),
-    ) );
-
-    $luoghi_options->add_field(array(
-        'id' => $prefix . 'posizione_mappa',
-        'name' => __('Visualizza mappa', 'design_scuole_italia'),
-        'desc' => __('Seleziona <b>Si</b> per visualizzare la mappa nella pagina di elenco dei luoghi.', 'design_comuni_italia'),
-        'type' => 'radio_inline',
-        'default' => 'false',
-        'options' => array(
-            'true' => __('Si', 'design_scuole_italia'),
-            'false' => __('No', 'design_scuole_italia'),
-        ),
-    ));
 }
