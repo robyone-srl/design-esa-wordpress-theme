e6ddc09ab470f81967ac6cc5ad599b07fa01c382
Gianni Nacu <gianninacu0@gmail.com>
2024-11-04 13:06:17 +0100

    Fix : permission settings in opzioni_comune

--- inc/admin/options/opzioni_comune.php
+++ inc/admin/options/opzioni_comune.php
@@ -16,7 +16,7 @@ function dci_register_comune_options(){
         'option_key'   => 'dci_options',
         'tab_group'    => 'dci_options',
         'tab_title'    => __('Configurazione Ente', "design_comuni_italia"),
-        'capability'    => 'manage_options',
+        'capability'    => 'manage_theme_options',
         'position'        => 2, // Menu position. Only applicable if 'parent_slug' is left empty.
         'icon_url'        => 'dashicons-admin-tools', // Menu icon. Only applicable if 'parent_slug' is left empty.
     );
