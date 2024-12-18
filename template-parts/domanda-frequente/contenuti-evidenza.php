1192b38a832b2f7e4202b276e05b32445529405d
Gianni Nacu <gianninacu0@gmail.com>
2024-12-18 14:33:57 +0100

    Fix title

--- template-parts/domanda-frequente/contenuti-evidenza.php
+++ template-parts/domanda-frequente/contenuti-evidenza.php
@@ -7,7 +7,7 @@ $schede = dci_get_meta('correlati', '_dci_domanda_frequente_', $post->ID);
 if ($schede && count($schede) > 0) { ?>
     
     <div class="container">
-        <h3 id="contenuti_evidenza" class="mb-lg-0 d-none">Contenuti in evidenza</h3>
+        <h3 id="contenuti_evidenza" class="mb-lg-0 d-none">Contenuti correlati</h3>
         <div class="mb-2">
             <div class="card-wrapper px-0 card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-6">
                 <?php $count = 1;
