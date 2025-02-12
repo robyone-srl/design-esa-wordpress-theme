29409f2c8863308d08af8c5af4e38a194e95b784
Gianni Nacu <gianninacu0@gmail.com>
2025-02-12 15:36:47 +0100

    Fix issue loading default image

--- inc/utils.php
+++ inc/utils.php
@@ -1214,9 +1214,16 @@ function dci_contains_element_with( $array, $key, $value) {
 if(!function_exists("dci_get_img")) {
     function dci_get_img( $url, $classes = '', $size = null ) {
         $img_post = get_post( attachment_url_to_postid($url) );
-        $scaled_url = wp_get_attachment_image_src($img_post -> ID, $size)[0] ?? $url;
-        $image_alt = get_post_meta( $img_post->ID, '_wp_attachment_image_alt', true);
-        $image_title = get_the_title( $img_post->ID );
+
+        $scaled_url = $url;
+        $image_alt = "";
+        $image_title = "";
+
+        if($img_post) {
+            $scaled_url = wp_get_attachment_image_src($img_post -> ID, $size)[0];
+            $image_alt = get_post_meta( $img_post->ID, '_wp_attachment_image_alt', true);
+            $image_title = get_the_title( $img_post->ID );
+        }
 
         $img = '<img src="'.$scaled_url.'" ';        
         if ($classes) $img .= 'class="'.$classes.'" ';
