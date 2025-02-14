7e07045a64a0a3797a32046a88f22c6339b9b405
Gianni Nacu <gianninacu0@gmail.com>
2025-02-14 14:26:02 +0100

    Fixed query issues

--- template-parts/ipab-comunica/notizie-carousel.php
+++ template-parts/ipab-comunica/notizie-carousel.php
@@ -6,43 +6,46 @@ $post_id = dci_get_option('notizia_evidenziata', 'comunica', true)[0] ?? null;
 $schede = dci_get_option('schede_evidenziate', 'comunica') ?: [];
 
 //Notizie in comunica
-$posts = null;
-$notizie_in_home = dci_get_option('notizie_in_comunica', 'comunica');
-if ($notizie_in_home && $notizie_in_home > 0) {
+$notizie_in_evidenza = dci_get_option('notizie_in_comunica', 'comunica');
+if ($notizie_in_evidenza && $notizie_in_evidenza > 0) {
     $args  = array(
         'post_type'      => 'notizia',
         'post_status'    => 'publish',
-        'posts_per_page' => $notizie_in_home,
+        'posts_per_page' => $notizie_in_evidenza,
         'orderby'        => 'date',
         'order'          => 'DESC',
         'exclude'        => [...($post_id ? [$post_id] : []), ...$schede],
     );
-    $posts = get_posts($args);
-    //$post  = array_shift( $posts  );
-}
 
-if ($post_id) {
-    array_unshift($posts, get_post($post_id));
+    if(get_posts($args)){
+
+        $posts = get_posts($args);
+        if ($post_id) {
+            array_unshift($posts, get_post($post_id));
+        }
+        if ($posts) { ?>
+            <div class="container px-0">
+                <h2 id="contenuti_evidenza" class="my-lg-4">Contenuti in evidenza</h2>
+                <div class="it-carousel-wrapper it-carousel-landscape-abstract splide mw-100" data-bs-carousel-splide>
+                    <div class="splide__track pt-0 px-0">
+                        <ul class="splide__list">
+ 
+                        <?php
+                            foreach ($posts as $post) {
+                            ?>
+                                <li class="splide__slide">
+                                    <div class="it-single-slide-wrapper">
+                                        <?php get_template_part("template-parts/ipab-comunica/notizia-hero"); ?>
+                                    </div>
+                                </li>
+                            <?php
+                            }
+                            ?>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        <?php }
+    }
 }
 ?>
-
-<div class="container px-0">
-    <h2 id="contenuti_evidenza" class="my-lg-4">Contenuti in evidenza</h2>
-    <div class="it-carousel-wrapper it-carousel-landscape-abstract splide mw-100" data-bs-carousel-splide>
-        <div class="splide__track pt-0 px-0">
-            <ul class="splide__list">
-                <?php
-                foreach ($posts as $post) {
-                ?>
-                    <li class="splide__slide">
-                        <div class="it-single-slide-wrapper">
-                            <?php get_template_part("template-parts/ipab-comunica/notizia-hero"); ?>
-                        </div>
-                    </li>
-                <?php
-                }
-                ?>
-            </ul>
-        </div>
-    </div>
-</div>
--- template-parts/ipab-comunica/notizie.php
+++ template-parts/ipab-comunica/notizie.php
@@ -17,10 +17,11 @@ if ($post_id) {
 // schede evidenziate, per escludere dalla query le notizie già evidenziate
 $schede = dci_get_option('schede_evidenziate', 'comunica') ?: [];
 
-//Notizie in homepage
-$posts = null;
+//Notizie 
 $notizie_in_comunica = dci_get_option('notizie_in_comunica', 'comunica');
-if ($notizie_in_comunica && $notizie_in_comunica > 0) {
+
+if (isset($notizie_in_comunica) && $notizie_in_comunica > 0) {
+
     $args  = array(
         'post_type'      => 'notizia',
         'post_status'    => 'publish',
@@ -32,67 +33,66 @@ if ($notizie_in_comunica && $notizie_in_comunica > 0) {
 
     if($giorni_per_filtro != "" || $giorni_per_filtro > 0) {
         $filter = array(
-                'date_query' => array(
-            		array(
-						'after' => '-'. $giorni_per_filtro . ' day',
-                		'inclusive' => true,
-            		),
+            'date_query' => array(
+            	array(
+					'after' => '-'. $giorni_per_filtro . ' day',
+                	'inclusive' => true,
             	),
+            ),
         );
 		$args = array_merge($args,$filter);       	
     }
+    if(get_posts($args)){
+        $posts = get_posts($args);
 
-    $posts = get_posts($args);
-    //$post  = array_shift( $posts  );
-}
+        $arrdata           = dci_get_data_pubblicazione_arr("data_pubblicazione", '_dci_notizia_', $post->ID);
+        $monthName         = date_i18n('M', mktime(0, 0, 0, $arrdata[1], 10));
+        $descrizione_breve = dci_get_meta("descrizione_breve", '_dci_notizia_', $post->ID);
+        $argomenti         = dci_get_meta("argomenti", '_dci_notizia_', $post->ID);
 
-$arrdata           = dci_get_data_pubblicazione_arr("data_pubblicazione", '_dci_notizia_', $post->ID);
-$monthName         = date_i18n('M', mktime(0, 0, 0, $arrdata[1], 10));
-$descrizione_breve = dci_get_meta("descrizione_breve", '_dci_notizia_', $post->ID);
-$argomenti         = dci_get_meta("argomenti", '_dci_notizia_', $post->ID);
+        $overlapping = "";
 
-$overlapping = "";
-
-if ($post_id || ($posts && is_array($posts) && count($posts) > 0)) {
-?>
-    <!-- Tag section is opened in home.php -->
-    <div class="section-content">
-        <div class="container">
-            <?php if ($post_id) {
-                ?> <h2 id="novita-in-evidenza" class="my-lg-5">Novità in evidenza</h2> <?php
-                $overlapping = "card-overlapping";
-                get_template_part("template-parts/home/notizia-hero");
-            }
-            if ($posts && is_array($posts) && count($posts) > 0) { ?>
-                <?php if (!$post_id) { ?>
-                    <div class="row row-title pt-30 pt-lg-60 pb-3">
-                        <div class="col-12 d-lg-flex justify-content-between">
-                             <h2 id="novita-in-evidenza" class="mb-lg-0">Notizie recenti</h2>
+        if ($post_id || ($posts && is_array($posts) && count($posts) > 0)) {
+        ?>
+            <div class="container">
+                <?php if ($post_id) {
+                    ?> <h2 id="novita-in-evidenza" class="my-lg-5">Novità in evidenza</h2> <?php
+                    $overlapping = "card-overlapping";
+                    get_template_part("template-parts/home/notizia-hero");
+                }
+                if ($posts && is_array($posts) && count($posts) > 0) { ?>
+                    <?php if (!$post_id) { ?>
+                        <div class="row row-title pt-30 pt-lg-60 pb-3">
+                            <div class="col-12 d-lg-flex justify-content-between">
+                                    <h2 id="novita-in-evidenza" class="mb-lg-0">Notizie recenti</h2>
+                            </div>
+                        </div>
+                    <?php } ?>
+                    <div class="row mb-2">
+                        <div class="card-wrapper <?php echo $overlapping; ?> card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3">
+                            <?php
+                            foreach ($posts as $post) {
+                                if ($post) {
+								    $scheda = $post;
+                                    get_template_part("template-parts/home/notizia-evidenza"); 
+                                }
+                            }
+                            ?>
                         </div>
                     </div>
-                <?php } ?>
-                <div class="row mb-2">
-                    <div class="card-wrapper <?php echo $overlapping; ?> card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3">
-                        <?php
-                        foreach ($posts as $post) {
-                            if ($post) {
-								$scheda = $post;
-                                get_template_part("template-parts/home/notizia-evidenza"); 
-                            }
-                        }
-                        ?>
+                    <div class="row my-4 justify-content-md-center">
+                        <a class="read-more pb-3" href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>">
+                            <button type="button" class="btn btn-outline-primary">Tutte le novità
+                                <svg class="icon">
+                                    <use xlink:href="#it-arrow-right"></use>
+                                </svg>
+                            </button>
+                        </a>
                     </div>
-                </div>
-                <div class="row my-4 justify-content-md-center">
-                    <a class="read-more pb-3" href="<?php echo dci_get_template_page_url("page-templates/novita.php"); ?>">
-                        <button type="button" class="btn btn-outline-primary">Tutte le novità
-                            <svg class="icon">
-                                <use xlink:href="#it-arrow-right"></use>
-                            </svg>
-                        </button>
-                    </a>
-                </div>
-            <?php } ?>
-        </div>
-    </div>
-<?php } ?>
+                <?php } ?>
+            </div>
+        <?php }
+    }
+}
+?>
+    
