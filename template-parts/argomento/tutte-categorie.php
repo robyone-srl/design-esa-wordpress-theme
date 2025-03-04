0bc2b926dc68b3c0100cf89f936c4c33b869c162
Gianni Nacu <gianninacu0@gmail.com>
2025-03-04 15:47:17 +0100

    Simplified template part management

--- template-parts/argomento/tutte-categorie.php
+++ template-parts/argomento/tutte-categorie.php
@@ -1,11 +1,7 @@
 <?php
-    global $argomento,$is_set_notizie, $is_set_event, $first_printed;
+    global $argomento, $first_printed;
 
-    $n_eventi = 3;
-        
-        get_template_part("template-parts/notizia/prossime-notizie");
-        get_template_part("template-parts/evento/prossimi-eventi");
-
+    $posts = dci_get_grouped_posts_by_term('argomenti-tutti', 'argomenti', $argomento->name, -1);
     if($posts) {
 ?>
 <section id="tutti" class="pb-5">
@@ -15,7 +11,7 @@
                 <div class="row align-items-center pb-2">
                     <h3 class="col-12 col-md-5 title-large-semi-bold pb-0 mb-0">
                     <?php 
-                    if($is_set_notizie || $is_set_event){echo "Gli altri contenuti";} else {echo"Tutti i contenuti"; }?>
+                    if($first_printed){echo "Gli altri contenuti";} else {echo"Tutti i contenuti"; }?>
                     </h3>
 
                     <div class="col-12 col-md-7 d-flex justify-content-start justify-content-md-end pb-2">
@@ -76,7 +72,7 @@
                             <?php 
                                 define('TIPI_POST', jsonToArray(get_template_directory()."/inc/tutte-tipologie.json")['tipi_tipologie']);
 
-                                foreach(TIPI_POST as $i){ 
+                                foreach(TIPI_POST as $i){
                                     $value = $i['value'];
                                     $name = $i['name'];
 
@@ -113,12 +109,6 @@
 		                case "domanda_frequente":
                                 get_template_part("template-parts/domanda-frequente/card-search");
 			                break;
-		                /*case "notizia":
-                                get_template_part("template-parts/".$post->post_type."/card-search");
-			                break;
-		                case "evento":
-                                get_template_part("template-parts/".$post->post_type."/card-search");
-			                break;*/
 		                case "unita_organizzativa":
                                 get_template_part("template-parts/unita-organizzativa/card-search");
 			                break;
@@ -126,10 +116,10 @@
                                 get_template_part("template-parts/".$post->post_type."/card-search");
 			                break;
                         case "sito_tematico":
-                            get_template_part("template-parts/sito-tematico/card-search");
+                                get_template_part("template-parts/sito-tematico/card-search");
 			                break;
                         case "page":
-                            get_template_part("template-parts/common/card-search");
+                                get_template_part("template-parts/common/card-search");
 			                break;
 		            } 
 
@@ -138,7 +128,9 @@
         </div>
     </div>
 </section>
-<?php } ?>
+<?php 
+    $first_printed = true;
+} ?>
 
 <script>
 $(document).ready(function() {
@@ -187,7 +179,7 @@ $(document).ready(function() {
             var existentButtons = $('.filters-list button[data-post-type="' + optionValue + '"]');
 
             if (!existentButtons.length) {
-                var newButtonHtml = `<button type="button" class="btn btn-extra-filter btn-outline-primary btn-sx w-150 mx-1" 
+                var newButtonHtml = `<button type="button" class="btn btn-extra-filter btn-outline-primary btn-xs w-150" 
                                         data-post-type="${optionValue}" 
                                         data-term="<?php echo $argomento->slug; ?>">
                                         ${optionLabel}
