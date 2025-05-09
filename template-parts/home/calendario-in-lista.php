2904e653c09b9f5dd5146d68a773f6e799e9133b
Gianni Nacu <gianninacu0@gmail.com>
2025-05-09 09:27:42 +0200

    Bugfix events in home

--- template-parts/home/calendario-in-lista.php
+++ template-parts/home/calendario-in-lista.php
@@ -3,15 +3,23 @@ global $recurrence_index;
 
 $quanti_eventi_mostrare = dci_get_option('quanti_eventi_mostrare', 'homepage') ?: 3;
 
+$url_eventi = dci_get_template_page_url("page-templates/eventi.php");
+
+$eventi_c = [];
+
+$oggi_t = time();
+
 $eventi = dci_get_eventi_calendar_array();
 usort($eventi, fn($a, $b) => $a['_dci_evento_data_orario_inizio'] <=> $b['_dci_evento_data_orario_inizio']);
 $eventi = array_slice($eventi, 0, $quanti_eventi_mostrare);
 
 
-$url_eventi = dci_get_template_page_url("page-templates/eventi.php");
+foreach($eventi as $evento){
+	if($evento['_dci_evento_data_orario_inizio'] >= $oggi_t || $evento['_dci_evento_data_orario_fine'] >= $oggi_t)
+		array_push($eventi_c, $evento);
+}
 
-
-if(count($eventi)>0){
+if(count($eventi_c)){
 ?>
 	<div class="section section-muted pb-90 pb-lg-50 px-lg-5 pt-0">
 		<div class="container">
@@ -22,16 +30,15 @@ if(count($eventi)>0){
 			</div>
 
 			<div class="row g-4">
-				<?php if (count($eventi)) { ?>
-
-					<?php
-					foreach ($eventi as $evento) {
+				<?php if (count($eventi_c)) { ?>
+				
+					<?php foreach($eventi_c as $evento){
 						$post = get_post($evento['id']);
 						$recurrence_index = $evento['indice_ricorrenza'] ?? -1;
-						get_template_part("template-parts/evento/card-full");
-					}
 
-					?>
+						$load_card_type = "evento";  
+						get_template_part("template-parts/evento/card-full");
+					} ?>
 					<div class="d-flex justify-content-end">
 
 						<a href="<?= $url_eventi ?>" class="btn btn-outline-primary full-mb" aria-label="Mostra tutti gli eventi" data-element="live-button-events">
