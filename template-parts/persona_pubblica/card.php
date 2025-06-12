57ec24fe0a7fd6ddbddbf328ea426c71b2344823
Gianni Nacu <gianninacu0@gmail.com>
2025-06-12 13:45:02 +0200

    Changed description with incarichi

--- template-parts/persona_pubblica/card.php
+++ template-parts/persona_pubblica/card.php
@@ -5,34 +5,41 @@ global $pp_id, $with_border, $hide_incarichi, $titleLevel;
 $persona = get_post($pp_id);
 
 $prefix = '_dci_persona_pubblica_';
+
 $descrizione_breve = dci_get_meta('descrizione_breve', $prefix, $persona->ID);
+
+    if ($descrizione_breve) {
+        $sottotitolo = $descrizione_breve;
+    } else if (!$hide_incarichi) {  
+        $inc_args = array(
+            'post_type' => 'incarico',
+            'meta_query' => array(
+                array(
+                    'key'     => '_dci_incarico_persona',
+                    'value'   => $persona->ID
+                ),
+            ),
+            'numberposts' => -1,
+            'post_status' => 'publish',
+            'orderby' => 'post_title',
+            'order' => 'ASC',
+        );
+        $inc_query = new WP_Query($inc_args);
+        $inc_list = get_posts($inc_args);
+
+        $incarichi = array();
+
+        foreach ($inc_list as $incarico) {
+            array_push($incarichi, $incarico->post_title);
+        }
+
+        $incarichi = array_unique($incarichi);
+    
+        $sottotitolo = implode(', ', $incarichi);
+ }
+
 $foto = dci_get_meta('foto', $prefix, $persona->ID);
 
-$inc_args = array(
-    'post_type' => 'incarico',
-    'meta_query' => array(
-        array(
-            'key'     => '_dci_incarico_persona',
-            'value'   => $persona->ID
-        ),
-    ),
-    'numberposts' => -1,
-    'post_status' => 'publish',
-    'orderby' => 'post_title',
-    'order' => 'ASC',
-);
-$inc_query = new WP_Query($inc_args);
-$inc_list = get_posts($inc_args);
-
-$incarichi = array();
-
-if(!$hide_incarichi){
-    foreach ($inc_list as $incarico) {
-        array_push($incarichi, $incarico->post_title);
-    }
-}
-
-$incarichi = array_unique($incarichi);
 
 if($titleLevel == "") $titleLevel = 4;
 ?>
@@ -46,10 +53,9 @@ if($titleLevel == "") $titleLevel = 4;
         </h<?php echo $titleLevel; ?>>
         <div class="card-text">
             <?php
-            if ($descrizione_breve) {
-                echo $descrizione_breve;
-            } else {
-                echo implode(', ', $incarichi);
+            
+            if ($sottotitolo) {
+                echo $sottotitolo;
             }
             ?>
         </div>
