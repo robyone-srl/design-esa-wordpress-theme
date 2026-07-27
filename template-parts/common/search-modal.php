300c2c4e5993d827c86c8c9d5e7b5397e4eb3cae
Marco Rubin <posta@marcorubin.it>
2026-07-27 14:07:51 +0200

    Fix search modal

--- template-parts/common/search-modal.php
+++ template-parts/common/search-modal.php
@@ -30,7 +30,7 @@
                       ></use>
                     </svg>
                   </button>
-                  <p><span class="h2"><?php _e("Cerca","design_comuni_italia"); ?></span></p>
+                  <h2><?php _e("Cerca","design_comuni_italia"); ?></h2>
                   <button
                     class="search-link d-none d-md-block"
                     type="button"
